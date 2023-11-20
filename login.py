82a63268d847f372799fcb29a6352c2d1e6d3002
artserjulietta <110678368+artserjulietta@users.noreply.github.com>
2023-11-20 19:29:29 +0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,50 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {} # { username : pokemon}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+
+        self.power = randint(30, 60)
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']["other"]['official-artwork']["front_default"])
+        else:
+            return "https://static.wikia.nocookie.net/anime-characters-fight/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/700?cb=20181021155144&path-prefix=ru"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"""Имя твоего покеомона: {self.name}
+сила покемона: {self.power}"""
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+
+
+
